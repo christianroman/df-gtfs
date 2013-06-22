b41cf6e0a359c89d3829765ba745403ed5fc5874
Christian Roman <chroman16@gmail.com>
2013-06-22 02:29:08 -0500

    Schema fixed

--- schema.sql
+++ schema.sql
@@ -38,16 +38,16 @@ CREATE TABLE stops
 
 CREATE TABLE routes
 (
-  route_id          text PRIMARY KEY,
   agency_id         text NULL REFERENCES agency(agency_id) ON DELETE CASCADE,
   route_short_name  text NOT NULL,
   route_long_name   text NOT NULL,
   route_desc        text NULL,
-  route_type        integer NOT NULL,
+  route_type        integer NULL,
   route_url         text NULL,
   route_color       text NULL,
   route_text_color  text NULL,
-  route_bikes_allowed text NULL
+  route_bikes_allowed text NULL,
+  route_id          text PRIMARY KEY
 );
 
 CREATE TABLE calendar
@@ -67,39 +67,39 @@ CREATE TABLE calendar
 CREATE TABLE shapes
 (
   shape_id          text PRIMARY KEY,
-  shape_pt_lat      wgs84_lat NOT NULL,
-  shape_pt_lon      wgs84_lon NOT NULL,
   shape_pt_sequence integer NOT NULL,
-  shape_dist_traveled double precision NULL
+  shape_dist_traveled double precision NULL,
+  shape_pt_lat      wgs84_lat NOT NULL,
+  shape_pt_lon      wgs84_lon NOT NULL
 );
 
 CREATE TABLE trips
 (
   route_id          text NOT NULL REFERENCES routes ON DELETE CASCADE,
   service_id        text NOT NULL REFERENCES calendar,
-  trip_id           text NOT NULL PRIMARY KEY,
-  trip_headsign     text NULL,
   trip_short_name   text NULL,
+  trip_headsign     text NULL,
+  route_short_name  text NULL,
   direction_id      boolean NULL,
   block_id          text NULL,
   shape_id          text NULL REFERENCES shapes,
-  route_short_name  text NULL,
   wheelchair_accessible text NULL,
-  trip_bikes_allowed text NULL
+  trip_bikes_allowed text NULL,
+  trip_id           text NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE stop_times
 (
   trip_id           text NOT NULL REFERENCES trips ON DELETE CASCADE,
+  stop_sequence     integer NOT NULL,
+  stop_id           text NOT NULL REFERENCES stops ON DELETE CASCADE,
   arrival_time      interval NOT NULL,
   departure_time    interval NOT NULL,
-  stop_id           text NOT NULL REFERENCES stops ON DELETE CASCADE,
-  stop_sequence     integer NOT NULL,
   stop_headsign     text NULL,
+  route_short_name  text NULL,
   pickup_type       integer NULL CHECK(pickup_type >= 0 and pickup_type <=3),
   drop_off_type     integer NULL CHECK(drop_off_type >= 0 and drop_off_type <=3),
-  shape_dist_traveled double precision NULL,
-  route_short_name  text NULL
+  shape_dist_traveled double precision NULL
 );
 
 CREATE TABLE frequencies
